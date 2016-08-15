32f94743d7166b2016ee63b40d1671aabbaff8d4
tiffanyjc <tiffanyc1225@gmail.com>
2016-08-15 13:45:02 -0700

    upload files

---
+++ main/main.js
@@ -0,0 +1,37 @@
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+//            document.getElementById('activeImg').src = e.target.result; 
+            document.getElementById('targetImg').src = e.target.result; 
+            document.getElementById('finalImg').src = e.target.result; 
+            document.getElementById('finalImgOverlay').src = e.target.result; 
+            document.getElementById('targetImgfeedback').src = e.target.result; 
+
+            document.getElementById('activeImg').src = e.target.result; 
+            
+            document.getElementById('outputLowTop').src = e.target.result; 
+            document.getElementById('outputMedTop').src = e.target.result; 
+            document.getElementById('outputHighTop').src = e.target.result; 
+            document.getElementById('outputLow').src = e.target.result; 
+            document.getElementById('outputMed').src = e.target.result; 
+            document.getElementById('outputHigh').src = e.target.result; 
+
+
+
+//            document.getElementById('discreteOutputBottom').src = e.target.result;
+//            document.getElementById('discreteOutputTop').src = e.target.result;
+            document.getElementById('continuousOutputBottom').src = e.target.result;
+            document.getElementById('continuousOutputTop').src = e.target.result;
+//            document.getElementById('activeImg').style.display = "block"
+            document.getElementById('nextEffects').disabled = false
+            document.getElementById('nextEffects').style.opacity = 1
+            document.getElementById('blah').src = e.target.result; 
+            document.getElementById('blah').style.width = "100%"; 
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+
+}
